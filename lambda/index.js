420023491180f4afdd5daa2c20a6054d99bc1219
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-08-06 18:56:52 +1000

    Updated lambda@edge code to modify request url

--- lambda/index.js
+++ lambda/index.js
@@ -3,5 +3,8 @@ exports.handler = (event, context, callback) => {
   console.log('TEST');
   console.log('event >>', JSON.stringify(event));
   console.log('context >>', JSON.stringify(context));
-  callback(null, event.Records[0].cf.request);
+
+  const request = event.Records[0].cf.request;
+  const newRequest = Object.assign({}, request, {uri: '/index.html'});
+  callback(null, newRequest);
 };
